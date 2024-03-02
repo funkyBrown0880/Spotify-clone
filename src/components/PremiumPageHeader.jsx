4afbb529ea7472456845a878136fe31fda345fcc
olufunke1011 <owoseniolufunke1011@gmail.com>
2024-03-02 17:52:21 +0100

    added the logo as a component and the li without an anchor tag

--- src/components/PremiumPageHeader.jsx
+++ src/components/PremiumPageHeader.jsx
@@ -1,9 +1,22 @@
 import './PremiumPageHeader.css'
+// import {Component}  from 'react'
+import {Logo} from './logo'
 export const PremiumPageHeader =() =>{
     return(
         <header className="header">
-            <div className="logo">logo</div>
-            <div className="menu">menu</div>
+            <div className="logo">
+                <Logo/>
+            </div>
+            <div className="menu">
+                <ul>
+                    <li>Premium</li>
+                    <li>Support</li>
+                    <li>Download</li>
+                    <li>|</li>
+                    <li>Sign up</li>
+                    <li>Log in</li>
+                </ul>
+            </div>
         </header>
     )
 }
---
+++ src/components/logo.jsx
@@ -0,0 +1,9 @@
+import SpotifyWhite from '../assets/images/SpotifyWhite.svg'
+export const Logo = () => {
+    return(
+        <a href='#home'>
+            <img src= {SpotifyWhite} alt=""/>
+        </a>
+        
+    )
+}
